f84cdf23931fcc88d1cc64de406fc8f19391d292
Denis Ibragimov <denis@mysale.com>
2019-10-08 12:05:35 +1100

    Update game status

--- EliteDangerousAPI/src/EliteDangerousAPI/Statuses/GameStatus.cs
+++ EliteDangerousAPI/src/EliteDangerousAPI/Statuses/GameStatus.cs
@@ -12,8 +12,10 @@ namespace NSW.EliteDangerous.API.Statuses
 
     public class GameStatus
     {
+        private readonly object _lock = new object();
+
         public GameStatusFlags Flags { get; private set; }
-        public bool Running => API.EliteDangerousAPI.GameRunning;
+        public bool Running => EliteDangerousAPI.GameRunning;
         public string Version { get; private set; }
         public string Language { get; private set; }
         public string MusicTrack { get; private set; }
@@ -22,47 +24,66 @@ namespace NSW.EliteDangerous.API.Statuses
         public GuiFocus GuiFocus { get; private set; } = GuiFocus.NoFocus;
 
 
-        internal GameStatus(API.EliteDangerousAPI api)
+        internal GameStatus(EliteDangerousAPI api)
         {
             Flags = GameStatusFlags.None;
 
             api.GameEvents.Status += (s, e) =>
             {
-                if (GuiFocus != e.GuiFocus)
+                lock (_lock)
                 {
-                    GuiFocus = e.GuiFocus;
-                    api.InvokeGameStatusChanged(this);
+                    if (GuiFocus != e.GuiFocus)
+                    {
+                        GuiFocus = e.GuiFocus;
+                        api.InvokeGameStatusChanged(this);
+                    }
                 }
             };
 
             api.GameEvents.FileHeader += (s, e) =>
             {
-                Version = $"{e.GameVersion} ({e.Build})";
-                Language = e.Language;
-                Flags |= GameStatusFlags.HeaderFound;
-                api.InvokeGameStatusChanged(this);
+                lock (_lock)
+                {
+                    Version = $"{e.GameVersion} ({e.Build})";
+                    Language = e.Language;
+                    Flags |= GameStatusFlags.HeaderFound;
+                    Flags &= ~GameStatusFlags.Shutdown;
+                    api.InvokeGameStatusChanged(this);
+                }
             };
 
             api.GameEvents.Shutdown += (s, e) =>
             {
-                Flags |= GameStatusFlags.Shutdown;
-                api.InvokeGameStatusChanged(this);
+                lock (_lock)
+                {
+                    Flags |= GameStatusFlags.Shutdown;
+                    api.InvokeGameStatusChanged(this);
+                }
             };
 
             api.GameEvents.Music += (s, e) =>
             {
-                if (MusicTrack != e.MusicTrack)
+                lock (_lock)
                 {
-                    MusicTrack = e.MusicTrack;
-                    api.InvokeGameStatusChanged(this);
+                    if (MusicTrack != e.MusicTrack)
+                    {
+                        MusicTrack = e.MusicTrack;
+                        Flags &= ~GameStatusFlags.Shutdown;
+                        api.InvokeGameStatusChanged(this);
+                    }
                 }
             };
 
             api.GameEvents.LoadGame += (s, e) =>
             {
-                GameMode = e.GameMode;
-                Horizons = e.Horizons ?? false;
-                api.InvokeGameStatusChanged(this);
+                lock (_lock)
+                {
+                    GameMode = e.GameMode;
+                    Horizons = e.Horizons ?? false;
+
+                    Flags &= ~GameStatusFlags.Shutdown;
+                    api.InvokeGameStatusChanged(this);
+                }
             };
         }
     }
